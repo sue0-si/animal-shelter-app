9199819524e5419c76f1fde5f6aabf52fed41c20
Mai, Vy <hmai35@gatech.edu>
2025-01-05 14:36:49 -0500

    version bump and made email clickable

--- src/components/ShelterList.jsx
+++ src/components/ShelterList.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-
 const ShelterList = ({ shelters }) => {
   const formatAddress = (address) => {
     const nullCheck = (value) => value == null ? "" : (value + ",");
-    return `${nullCheck(address.address1)} ${nullCheck(address.address2)} ${nullCheck(address.city)} ${nullCheck(address.state)} ${nullCheck(address.postcode).replace(',', '')}`;
-  };  
+    return `${nullCheck(address.address1)} ${nullCheck(address.address2)} ${nullCheck(address.city)} ${nullCheck(address.state).slice(0,-1)} ${nullCheck(address.postcode).slice(0, -1)}`;
+  };
+  const EmailLink = ({ email }) => {
+    return <a href={`mailto:${email}`}>{email}</a>;
+  };
   return (
     <div>
       <h2>Available Animal Adoption Shelters</h2>
@@ -16,7 +18,7 @@ const ShelterList = ({ shelters }) => {
               <h3>{shelter.name}</h3>
               <p>{formatAddress(shelter.address)}</p>
               <p>{shelter.phone}</p>
-              <p>{shelter.email}</p>
+              <EmailLink email={shelter.email}/>
             </li>
           ))}
         </ul>
